43a3baaec555622be5a57d9093ea28f57d92cf04
moritztng <moritz@tenstorrent.com>
2026-08-12 10:28:08 +0000

    bench(boltz2): raise the L1/L2 A/B lock wait to 5400 s -- five workers are queued on the box benchlock and 1800 s expires before the queue drains

--- perf/b2deep/run_l1l2.sh
+++ perf/b2deep/run_l1l2.sh
@@ -5,7 +5,7 @@
 WT=/home/ttuser/.coworker/wt/boltz2-512aa-deep-perf
 cd $WT || exit 1
 export TT_VISIBLE_DEVICES=1 TT_BIO_LEASE_HOLDER=worker:boltz2-512aa-deep-perf PYTHONPATH=$WT
-export BENCHLOCK_WAIT_S=1800 BENCHLOCK_LOAD_WAIT_S=300
+export BENCHLOCK_WAIT_S=5400 BENCHLOCK_LOAD_WAIT_S=300
 /home/ttuser/.coworker/scripts/benchlock.sh boltz2-512aa-deep-perf -- \
   /home/ttuser/tt-bio-dev/env/bin/python3 -u perf/other512/fold_ab_multi.py --model boltz2 \
     --sizes 512 --arms on,s3,l1,both,nos2,on,nos2,both,l1,s3,on,nos2 \
