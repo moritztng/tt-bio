e2a38e9a1475d5494c5b4a841d59900691c6f561
Moritz Thüning <moritz.thuening@gmail.com>
2026-09-14 06:20:11 +0000

    gate on cards 1 and 2: card 3 stalled twice this pass

--- perf/b2z2_gate/run_gate_zinitship_rebased_2card.sh
+++ perf/b2z2_gate/run_gate_zinitship_rebased_2card.sh
@@ -7,9 +7,9 @@ cd $W
 export PYTHONPATH=$W
 export OPENDDE_DOCKQ_PYTHON=/home/ttuser/dockqenv/bin/python3
 export TT_BIO_LEASE_HOLDER=worker:b2z2-zinit-ship
-export TT_BIO_LEASE_CARDS=1,3
+export TT_BIO_LEASE_CARDS=1,2
 /home/ttuser/tt-bio-dev/env/bin/python3 -u scripts/full_parity_gate.py \
-  --workers localhost:1,localhost:3 \
+  --workers localhost:1,localhost:2 \
   --workdir $W/perf/b2z2_gate/zinitship_rebased_work \
   --out $W/perf/b2z2_gate/gate_zinitship_rebased.json
 echo GATEDONE
